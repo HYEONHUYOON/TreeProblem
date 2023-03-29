36cfd88209ab9ede206a4ba654ad045ed7b98643
HYEONHUYOON <101074004+HYEONHUYOON@users.noreply.github.com>
2023-03-29 11:13:11 +0900

    Add files via upload
    Tree

---
+++ ProblemTree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tree
+{
+    class Node
+    {
+        int nodeVal;
+        Node[] childNode;
+        int childNodeCount = 0;
+
+        public Node(int nodeName)
+        {
+            this.nodeVal = nodeName;
+        }
+
+        public Node(int nodeName,int parentNodeVal)
+        {
+            this.nodeVal = nodeName;
+        }
+
+        public void MakeChildNode(int CnodeVal)
+        {
+            childNode[childNodeCount] = new Node(CnodeVal,this.nodeVal);
+            childNodeCount++;
+        }
+    }
+}
